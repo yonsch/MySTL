3da1a0b3d8ad0505c7d5b1cf66ca088f2aa09b99
Yonatan Schachter <yonatan.schachter@gmail.com>
2017-07-12 17:04:43 +0300

    fixed things

--- MySTL/MySTL.cpp
+++ MySTL/MySTL.cpp
@@ -6,33 +6,8 @@
 using namespace std;
 
 int main() {
-	STLMesh m("C:\\Users\\Yonch\\Desktop\\pyr.STL");
-	STLMesh m2 = m*2.f;
-	m2.toFile("C:\\Users\\Yonch\\Desktop\\pyr2.stl");
-	/*
-	Pyramid:
 
-	Vector3D a = Vector3D(0, 0, 0);
-	Vector3D b = Vector3D(0, 0, 20);
-	Vector3D c = Vector3D(0, 20, 20);
-	Vector3D d = Vector3D(20, 0, 20);
-	Face f1 = Face(a, b, c);
-	Face f2 = Face(d, b, a);
-	Face f3 = Face(c, b, d);
-	Face f4 = Face(c, d, a);
-	vector<Face> faces;
-	faces.push_back(f1);
-	faces.push_back(f2);
-	faces.push_back(f3);
-	faces.push_back(f4);
-	STLMesh m;
-	m.faces = faces;
-	m.path = "C:\\Users\\Yonch\\Desktop\\f.stl";
-	m.header = "please work";
-	m.triCount = m.faces.size();
-	m.toFile("C:\\Users\\Yonch\\Desktop\\f.stl");
 	
-	*/
 	/*
 	Vector3D a = Vector3D(40, 30, 0);
 	Vector3D b = Vector3D(0, 0, 0);
