2b13ec1e51432c57ede44e8a9638b400800858f3
Michael Gur <michael@thegur.net>
2017-07-16 01:06:01 +0300

    Update Shader.cpp

--- MySTL/Shader.cpp
+++ MySTL/Shader.cpp
@@ -1,7 +1,7 @@
-#include "Shader.h"
-#include "SDL.h"
-#include "glew.h"
-#include <string>
+#include "shader.h"
+#include <glew.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -12,6 +12,17 @@ int createShader(const string& shader, int type) {
 	glShaderSource(id, 1, &c_str, NULL);
 	glCompileShader(id);
 
+	int compiled;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &compiled);
+	if (!compiled) {
+		int length = 0;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+
+		char* log = new char[length];
+		glGetShaderInfoLog(id, length, &length, &log[0]);
+		cout << log << endl;
+	}
+
 	return id;
 }
 
@@ -19,19 +30,32 @@ Shader::Shader(const string& vertex, const string& fragment) {
 	int vertexID = createShader(vertex, GL_VERTEX_SHADER);
 	int fragmentID = createShader(fragment, GL_FRAGMENT_SHADER);
 
+	id = glCreateProgram();
+
 	glAttachShader(id, vertexID);
 	glAttachShader(id, fragmentID);
 
 	glLinkProgram(id);
 	glValidateProgram(id);
 
+	int linked;
+	glGetProgramiv(id, GL_LINK_STATUS, &linked);
+	if (!linked) {
+		int length;
+		glGetProgramiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* log = new char[length];
+
+		glGetProgramInfoLog(id, length, &length, log);
+		cout << log << endl;
+	}
+
+
 	glDeleteShader(vertexID);
 	glDeleteShader(fragmentID);
-	
+
 	glBindAttribLocation(id, 0, "position");
 }
 
 void Shader::bind() { glUseProgram(id); }
 void Shader::unbind() { glUseProgram(0); }
 
-Shader::Shader() {}
